4a07f996d82246e3d1b7d91a2d6e1dd11a5c0ea0
hexuewen <hexuewen2008@126.com>
2021-07-24 20:31:23 +0800

    将验证码图片生成base64格式

--- dongbao-mall/dongbao-application/dongbao-portal-web/src/main/java/com/xuewen/dongbao/portal/web/controller/VerifyCodeController.java
+++ dongbao-mall/dongbao-application/dongbao-portal-web/src/main/java/com/xuewen/dongbao/portal/web/controller/VerifyCodeController.java
@@ -1,6 +1,7 @@
 package com.xuewen.dongbao.portal.web.controller;
 
 import com.xuewen.dongbao.common.base.annotations.TokenCheck;
+import com.xuewen.dongbao.common.base.result.ResultWrapper;
 import com.xuewen.dongbao.portal.web.code.ImageCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,9 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/code")
@@ -51,5 +54,37 @@ public class VerifyCodeController {
         }else {
             return "验证码校验不通过";
         }
+    }
+
+
+
+    @GetMapping("/generatorBase64")
+    @TokenCheck(required=false)
+    public ResultWrapper generatorCodeBase64(HttpServletRequest request, HttpServletResponse response) {
+        String s = "";
+        try {
+            ImageCode imageCode = ImageCode.getInstance();
+
+            String code = imageCode.getCode();
+            ByteArrayInputStream image = imageCode.getImage();
+
+            request.getSession().setAttribute(attrName, code);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] bytes = new byte[1024];
+            int r = 0;
+
+            while((r=image.read(bytes, 0, 1024)) > 0) {
+                byteArrayOutputStream.write(bytes, 0, r);
+            }
+
+            byte[] data = byteArrayOutputStream.toByteArray();
+            s = Base64.getEncoder().encodeToString(data);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return ResultWrapper.getSuccessBuilder().data(s).build();
     }
 }
